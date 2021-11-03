721cdea4ecc64c9903eec7b472e1d27cd5c4cc00
Stanislas Chevallier <contact@stanislaschevallier.fr>
2021-11-03 23:38:57 +0100

    Initial Commit

---
+++ Migrator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Migrator
+//
+//  Created by Stanislas Chevallier on 03/11/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
